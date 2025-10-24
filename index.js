16339b0aed4f5c29ed451e10ec20b0fe72c2418e
Souleymane DIALLO <sdley2020@gmail.com>
2025-10-24 22:27:43 +0000

    BREAKING CHANGE: function 3 arguments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
-const calc = require('./lib/calc');
-console.log('semver-demo-npm v1.0.0');
-console.log('2 + 3 =', calc.add(2,3));
+const calc = require("./lib/calc");
+console.log("semver-demo-npm v2.0.0");
+console.log("2 + 3 + 4 =", calc.add(2, 3, 4));
+console.log("2 * 3 * 4 =", calc.multiply(2, 3, 4));
